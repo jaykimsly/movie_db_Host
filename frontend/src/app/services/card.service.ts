bb6b893b8225d8c6f7135a0825d599b4ed3dc720
DA_M1_44 <da_m1_44@Academy-learners-MacBook-Air-2.local>
2022-07-05 15:56:19 +0200

    push

--- frontend/src/app/services/card.service.ts
+++ frontend/src/app/services/card.service.ts
@@ -7,9 +7,9 @@ import { Injectable } from '@angular/core';
 })
 export class CardService {
   
-  baseUrl = "https://api.themoviedb.org/3/discover/movie?api_key=80a33826576591aad78e11cfbde39527";
-  popUrl = "https://api.themoviedb.org/3/discover/movie?api_key=80a33826576591aad78e11cfbde39527&language=en-US&sort_by=popularity.asc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate";
-  sugUrl = " https://api.themoviedb.org/3/discover/movie?api_key=80a33826576591aad78e11cfbde39527&&primary_release_year=2020";
+  baseUrl = "https://api.themoviedb.org/3/discover/movie?api_key=80a33826576591aad78e11cfbde39527&&primary_release_year=2021";
+  popUrl = "https://api.themoviedb.org/3/discover/movie?api_key=80a33826576591aad78e11cfbde39527";
+  sugUrl = " https://api.themoviedb.org/3/discover/movie?api_key=80a33826576591aad78e11cfbde39527&&primary_release_year=2019";
   constructor(private http: HttpClient) { }
   
 
