b265b6372efcb25f4936da0469f5b5baf726d8aa
DA_M1_44 <da_m1_44@Academy-learners-MacBook-Air-2.local>
2022-07-06 13:26:42 +0200

    Pushing my latest work

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,11 +9,11 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { SplashComponent } from './components/splash/splash.component';
 import { LatestMoviesPageComponent } from './components/latest-movies-page/latest-movies-page.component';
 import { SuggestionsPageComponent } from './components/suggestions-page/suggestions-page.component';
-/*import { ActionComponent } from './components/action/action.component';
+import { ActionComponent } from './components/action/action.component';
 import { ComedyComponent } from './components/comedy/comedy.component';
 import { DramaComponent } from './components/drama/drama.component';
 import { FamilyComponent } from './components/family/family.component';
-import { KidsComponent } from './components/kids/kids.component';*/
+import { KidsComponent } from './components/kids/kids.component';
 
 const routes: Routes = [
   {path: 'sign-in', component: SignInComponent},
@@ -23,12 +23,12 @@ const routes: Routes = [
   {path: '', component: CardComponent},
   {path: 'latest-movies', component: LatestMoviesPageComponent},
   {path: 'suggestions', component: SuggestionsPageComponent},
-  {path: 'splash', component: SplashComponent}
-  /*{path: 'action', component: ActionComponent},
+  {path: 'splash', component: SplashComponent},
+  {path: 'action', component: ActionComponent},
   {path: 'comedy', component: ComedyComponent},
   {path: 'drama', component: DramaComponent},
   {path: 'family', component: FamilyComponent},
-  {path: 'kids', component: KidsComponent}*/
+  {path: 'kids', component: KidsComponent}
 ];
 
 @NgModule({
